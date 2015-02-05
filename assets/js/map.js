19910458fb8a8b210878206290a6ffdca08a2a4f
Michael Waterworth <mike@3equals.co.uk>
2015-02-05 14:56:40 +0000

    Add edge marker and restore view
    Additionally move custom css file further down

--- assets/js/map.js
+++ assets/js/map.js
@@ -2,19 +2,41 @@
 	var expo = {};
 
     function init(){
-        expo.map = L.map('mapdiv', '', {zoomControl: false}).setView([50.188214, -5.293300], 18);
+        expo.map = L.map('mapdiv', '', {zoomControl: false});
+
+        if (!expo.map.restoreView()) {//Load map from previous state - Remembered between loads
+          expo.map.setView([50.188214, -5.293300], 18);//Set default map state
+        }
+
+        L.edgeMarker().addTo(expo.map)
+
         expo.oms = new OverlappingMarkerSpiderfier(expo.map);
 
         expo.layers.satellite = L.tileLayer('http://{s}.tiles.mapbox.com/v3/paperclipmonkey.kggafd49/{z}/{x}/{y}.png', {
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-            maxZoom: 18
+            maxZoom: 19
         }).addTo(expo.map);
 
-        expo.layers.road = L.tileLayer('http://{s}.tiles.mapbox.com/v3/paperclipmonkey.kgon2gjp/{z}/{x}/{y}.png', {
-            attribution: 'Road map &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'});
+        expo.layers.road = L.tileLayer.wms('http://ogc.bgs.ac.uk/cgi-bin/BGS_Bedrock_and_Superficial_Geology/wms?', {
+          format: 'image/png',
+          transparent: true,
+         // layers: 'BGS.50k.Bedrock',
+          layers: 'GBR_BGS_625k_BA',
+          attribution: "BGS",
+          //opacity: 1.0,
+          //maxZoom: 13,
+          //minZoom: 13,
+          //minNativeZoom: 13
+        }).addTo(expo.map);
+
+        // expo.layers.road = L.tileLayer('http://{s}.tiles.mapbox.com/v3/paperclipmonkey.kgon2gjp/{z}/{x}/{y}.png', {
+        //     attribution: 'Road map &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'});
 
         expo.layers.old = L.tileLayer('http://nls-1.tileserver.com/os_6_inch_gb/{z}/{x}/{y}.jpg', {
-            attribution: 'OS Map provided by; <a href="http://maps.nls.uk/os/6inch-england-and-wales/index.html">NLS</a>'});
+            //opacity: 1.0,
+            //subdomains ['',''],
+            attribution: 'OS Map provided by; <a href="http://maps.nls.uk/os/6inch-england-and-wales/index.html">NLS</a>'
+        });
 
 
         var quoitIcon = L.icon({
@@ -23,6 +45,7 @@
             iconSize: [75, 38],
             iconAnchor: [50, 25],
             popupAnchor: [-3, -50],
+            //opacity: 1.0,
             // shadowUrl: 'my-icon-shadow.png',
             // shadowRetinaUrl: 'my-icon-shadow@2x.png',
             // shadowSize: [68, 95],
