048f78e843692b923d627bb1eb481e3b2b386ed0
Michael Waterworth <mike@3equals.co.uk>
2015-02-10 04:36:27 +0000

    Use Sidebar and overlay sidebar

--- assets/js/map.js
+++ assets/js/map.js
@@ -8,26 +8,21 @@
           expo.map.setView([50.188214, -5.293300], 18);//Set default map state
         }
 
-        L.edgeMarker().addTo(expo.map)
+        L.edgeMarker().addTo(expo.map);
 
-        expo.oms = new OverlappingMarkerSpiderfier(expo.map);
+        expo.sidebar = L.control.sidebar('walkholder', {
+            position: 'left'
+        });
+
+        expo.map.addControl(expo.sidebar);
+
+
+        expo.oms = new OverlappingMarkerSpiderfier(expo.map, {keepSpiderfied: true});
 
         expo.layers.satellite = L.tileLayer('http://{s}.tiles.mapbox.com/v3/paperclipmonkey.kggafd49/{z}/{x}/{y}.png', {
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
             maxZoom: 19
         }).addTo(expo.map);
-
-        // expo.layers.road = L.tileLayer.wms('http://ogc.bgs.ac.uk/cgi-bin/BGS_Bedrock_and_Superficial_Geology/wms?', {
-        //   format: 'image/png',
-        //   transparent: true,
-        //  // layers: 'BGS.50k.Bedrock',
-        //   layers: 'GBR_BGS_625k_BA',
-        //   attribution: "BGS",
-        //   opacity: 0.45,
-        //   //maxZoom: 13,
-        //   //minZoom: 13,
-        //   //minNativeZoom: 13
-        // }).addTo(expo.map);
 
         expo.layers.road = L.tileLayer('http://{s}.tiles.mapbox.com/v3/paperclipmonkey.kgon2gjp/{z}/{x}/{y}.png', {
             attribution: 'Road map &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'});
@@ -35,8 +30,28 @@
         expo.layers.old = L.tileLayer('http://nls-1.tileserver.com/os_6_inch_gb/{z}/{x}/{y}.jpg', {
             //opacity: 1.0,
             //subdomains ['',''],
+            opacity: 0.5,
             attribution: 'OS Map provided by; <a href="http://maps.nls.uk/os/6inch-england-and-wales/index.html">NLS</a>'
         });
+
+        expo.layers.geology = L.tileLayer.wms('http://ogc.bgs.ac.uk/cgi-bin/BGS_Bedrock_and_Superficial_Geology/wms?', {
+          format: 'image/png',
+          transparent: true,
+         // layers: 'BGS.50k.Bedrock',
+          layers: 'GBR_BGS_625k_BA',
+          attribution: "BGS",
+          opacity: 0.5,
+          //maxZoom: 13,
+          //minZoom: 13,
+          //minNativeZoom: 13
+        });
+
+        expo.layers.markers = {
+          'other': L.featureGroup().addTo(expo.map),
+          'find': L.featureGroup().addTo(expo.map),
+          'plant': L.featureGroup(),
+          'keysite': L.featureGroup().addTo(expo.map)
+        };
 
 
         var quoitIcon = L.icon({
@@ -53,7 +68,64 @@
         });
 
         var m = L.marker([50.188214, -5.293300], {icon: quoitIcon}).addTo(expo.map);
-    }
+
+        var baseLayers = [
+          {
+            name: "Satellite",
+            layer: expo.layers.satellite
+          },
+          { 
+            name: "Road",
+            layer: expo.layers.road
+          }
+        ];
+
+        var overlayLayers = [
+          {
+            group: "Overlays",
+            layers: [
+              {
+                name: "Old OS",
+                layer: expo.layers.old,
+                opacity: true
+              },
+              { 
+                name: "Geology",
+                layer: expo.layers.geology,
+                opacity: true
+              }
+            ]
+          },
+          {
+            group: "Markers",
+            layers: [
+              {
+                name: "Keysite",
+                layer: expo.layers.markers.keysite,
+                icon: '<i class="marker-icon marker-brown"></i>'
+              },
+              {
+                name: "Other",
+                layer: expo.layers.markers.other,
+                icon: '<i class="marker-icon marker-orange"></i>'
+              },
+              { 
+                name: "Finds",
+                layer: expo.layers.markers.find,
+                icon: '<i class="marker-icon marker-brown"></i>'
+              },
+              {
+                name: "Plant",
+                layer: expo.layers.markers.plant,
+                icon: '<i class="marker-icon marker-green"></i>'
+              }
+            ]
+          }
+        ];
+
+        var panelLayers = new L.Control.PanelLayers(baseLayers, overlayLayers);
+        expo.map.addControl(panelLayers);
+    }8
 
     expo.init = init;
 
